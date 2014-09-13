d8d00539658b3db805ceb2d42b1520c4354247fb
carlos guzman <guzmca@gmail.com>
2014-09-13 14:01:11 -0400

    Now everything works but the initial date

--- schedule.py
+++ schedule.py
@@ -1,5 +1,46 @@
 from datetime import datetime
 from event import Event
+import StringIO
+
+def decode_time(raw):
+    times = raw.split(":")
+    if times[1][-2:] == "PM" and times[0] != "12":
+        hour = str(int(times[0])+12)
+    else:
+        hour = times[0]
+
+    if len(hour)<2:
+        hour = "0"+hour
+
+    return hour+times[1][:2]+"00"
+
+
+def parse(text):
+    # input = StringIO.StringIO()
+    # if input.readline()[:4].lower() == "this":
+    #     input.readline()
+    # next = input.readline()
+    
+    events = []
+    
+    events_str = text.split("Academic Calendar Deadlines\n")
+    events_str.pop(0)
+    for e in events_str:
+        event = e.split("\n")
+
+        times = event[2].upper().split(" ", 1)
+
+        start = decode_time(times[1].split(" - ")[0])
+        end = decode_time(times[1].split(" - ")[1])
+
+        events.append(Event(event[0], 
+            event[3].replace(" Room:", "- ").lstrip("Bldg:"),
+             start, end, [times[0][i:i+2] for i in range(0, len(times[0]), 2)], event[1]))
+
+    print len(events), " events have been parsed\n"
+
+    return events
+
 
 def write_calendar(
         events, file=open("schedule.ics", "w"), cal_name="My Schedule"):
@@ -29,32 +70,50 @@ def write_calendar(
         "END:STANDARD\r\n"+
         "END:VTIMEZONE\r\n");
 
-    d = datetime.now().strftime("%Y%m%dT%H%M%SZ")
+    current_date = datetime.now().strftime("%Y%m%dT%H%M%SZ")
+
 
     for event in events:
+        weekdays = ""
+        for day in event.weekdays:
+            weekdays += day+","
+        weekdays = weekdays[:-1]
+
         w.write("BEGIN:VEVENT\r\n"+
             "DTSTART;TZID=America/New_York:20140910T"+event.start+"\r\n"+
             "DTEND;TZID=America/New_York:20140910T"+event.end+"\r\n"+
-            "RRULE:FREQ=WEEKLY;UNTIL=20141218T170000Z;BYDAY=MO,TU,WE,TH\r\n"+
-            "CREATED:"+d+"\r\n"+
-            "DESCRIPTION:\r\n"+
-            "LAST-MODIFIED:"+d+"\r\n"+
-            "LOCATION:\r\n"+
+            "RRULE:FREQ=WEEKLY;UNTIL=20141218T170000Z;BYDAY="+weekdays+"\r\n"+
+            "CREATED:"+current_date+"\r\n"+
+            "DESCRIPTION:"+event.description+"\r\n"+
+            "LAST-MODIFIED:"+current_date+"\r\n"+
+            "LOCATION:"+event.place+"\r\n"+
             "SEQUENCE:0\r\n"+
             "STATUS:CONFIRMED\r\n"+
-            "SUMMARY:Bus\r\n"+
+            "SUMMARY:"+event.name+"\r\n"+
             "TRANSP:OPAQUE\r\n"+
             "END:VEVENT\r\n")
 
     w.write("END:VCALENDAR")
     w.close()
 
+    print len(events), " events have been successfully added to the calendar\n"
+
     return
 
 
-#r = open("nyubuse", "r")
+r = open("schedule", "r")
+events = parse(r.read())
 
-events = [Event("Name", "Somewhere", "012345", "123456", ["MO", "WE"])]
+    # "This Week's Schedule\n"+
+    # "Class   Schedule\n"+
+    # "Academic Calendar Deadlines\n"+
+    # "CORE-UA 536-001\n"+
+    # "LEC (18164)\n"+
+    # "TuTh 9:30AM - 10:45AM\n"+
+    # "Bldg:SILV  Room:207")
+
+
+#events = [Event("Name", "Somewhere", "012345", "123456", ["MO", "WE"])]
 
 
 w = open("test.ics", "w")
